c23ce2ace52a5a7b46ae31c62fb506ec2ad6375b
icw0201 <icw0207@naver.com>
2026-02-24 22:07:29 +0900

    fix: 16kb 버전 비정상 종료 문제제

--- android/app/src/main/java/com/simpleknitcounter/MainActivity.kt
+++ android/app/src/main/java/com/simpleknitcounter/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.simpleknitcounter
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
+
+  /**
+   * react-native-screens 권장: Fragment 복원을 막아 "context detached from activity" /
+   * "Screen fragments should never be restored" 크래시 방지 (배경 복귀, 16KB 페이지 크기 기기 등).
+   * https://github.com/software-mansion/react-native-screens/issues/17
+   */
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
