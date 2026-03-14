4c2b61273ca27f3dcfe1ebd8b3c3816f9767ab02
icw0201 <icw0207@naver.com>
2026-03-14 21:10:36 +0900

    build: react-native-keyevent 라이브러리 추가 및 초기 설정

--- android/app/src/main/java/com/simpleknitcounter/MainActivity.kt
+++ android/app/src/main/java/com/simpleknitcounter/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.simpleknitcounter
 
 import android.os.Bundle
+import android.view.KeyEvent
+import com.github.kevinejohn.keyevent.KeyEventModule
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -15,6 +17,16 @@ class MainActivity : ReactActivity() {
    */
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(null)
+  }
+
+  override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
+    KeyEventModule.getInstance()?.onKeyDownEvent(keyCode, event)
+    return super.onKeyDown(keyCode, event)
+  }
+
+  override fun onKeyUp(keyCode: Int, event: KeyEvent): Boolean {
+    KeyEventModule.getInstance()?.onKeyUpEvent(keyCode, event)
+    return super.onKeyUp(keyCode, event)
   }
 
   /**
