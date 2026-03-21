e57fc53940745371b1697fa5bcf5a710e362fb66
icw0201 <icw0207@naver.com>
2026-03-21 23:06:04 +0900

    design: 구분선 체리 아이콘 추가

--- src/components/counter/RecordListModal.tsx
+++ src/components/counter/RecordListModal.tsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import { Text, View } from 'react-native';
+import { Cherry } from 'lucide-react-native';
 import { ScrollModal } from '@components/common/modals';
 
 interface RecordListModalProps {
@@ -31,8 +32,12 @@ const RecordListModal: React.FC<RecordListModalProps> = ({
               </Text>
             </View>
             {index === 2 && (
-              <View className="my-1.5">
-                <View className="h-px w-4/5 self-center bg-red-orange-400" />
+              <View className="my-1.5 w-full flex-row items-center justify-center">
+                <View className="h-px w-20 bg-red-orange-400" />
+                <View className="mx-2">
+                  <Cherry size={14} color="#ff6b67" />
+                </View>
+                <View className="h-px w-20 bg-red-orange-400" />
               </View>
             )}
           </Fragment>
