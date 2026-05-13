d5a41d20a065d4b36f9f74339e673e9f96c21d59
icw0201 <icw0207@naver.com>
2026-05-13 12:18:49 +0900

    chore: bump version to 1.5.0-internal.15

--- src/screens/Setting.tsx
+++ src/screens/Setting.tsx
@@ -39,7 +39,7 @@ const Settings = () => {
 
           <SettingsAppInfo />
 
-          <SettingsVersion version="1.5.0-internal.14" />
+          <SettingsVersion version="1.5.0-internal.15" />
         </ScrollView>
       </KeyboardAvoidingView>
     </SafeAreaView>
