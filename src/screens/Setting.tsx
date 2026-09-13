7530c8c471f6aded931a48e0514bc4f8dd446c9f
icw0201 <icw0207@naver.com>
2026-09-14 00:25:17 +0900

    chore: bump version to 1.6.2-internal.2

--- src/screens/Setting.tsx
+++ src/screens/Setting.tsx
@@ -51,7 +51,7 @@ const Settings = () => {
           <SettingsAppInfo />
 
           <SettingsVersion
-            version="1.6.2-internal.1"
+            version="1.6.2-internal.2"
             onLongPress={() => navigation.navigate('PremiumPurchase')}
           />
         </ScrollView>
